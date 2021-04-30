72e6a4ceddcf128b79268a369ee8cffd68b1b3c8
shantam sultania <48251619+shantamsultania@users.noreply.github.com>
2021-04-30 15:48:20 +0530

    30 days of Java - Day 14 Code

--- day12java/src/main/java/com/example/day12java/AwsHelper/AwsCredential.java
+++ day12java/src/main/java/com/example/day12java/AwsHelper/AwsCredential.java
@@ -5,7 +5,6 @@ import lombok.Data;
 @Data
 public class AwsCredential {
 
-    private String AccessKey = "AKIARDBOONKXTFAM6N44";
-    private String SecretKey = "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";
-
+    private String AccessKey ;
+    private String SecretKey ;
 }
