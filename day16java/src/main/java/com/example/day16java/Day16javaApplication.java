405b8a8b1b6b5fa0e46915e2e0569f1f66059681
shantam sultania <48251619+shantamsultania@users.noreply.github.com>
2021-05-02 21:56:13 +0530

    30 days of Java - Day 16 Code

---
+++ day16java/src/main/java/com/example/day16java/Day16javaApplication.java
@@ -0,0 +1,13 @@
+package com.example.day16java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day16javaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day16javaApplication.class, args);
+    }
+
+}
---
+++ day16java/src/test/java/com/example/day16java/Day16javaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day16java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day16javaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
