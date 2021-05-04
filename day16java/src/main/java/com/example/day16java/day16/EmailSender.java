fb607ad1afeb5c96b82941f95a880c7c6fa5cb10
shantam sultania <48251619+shantamsultania@users.noreply.github.com>
2021-05-04 14:41:56 +0530

    30 days of Java - Day 18 Code

--- day16java/src/main/java/com/example/day16java/day16/EmailSender.java
+++ day16java/src/main/java/com/example/day16java/day16/EmailSender.java
@@ -27,37 +27,38 @@ public class EmailSender {
         props.put("mail.smtp.starttls.enable", false);
 
 
-//        try {
-//            // creating session object to get properties
-//
-//            Session session = Session.getDefaultInstance(props,
-//                    new Authenticator() {
-//                        protected PasswordAuthentication getPasswordAuthentication() {
-//                            return new PasswordAuthentication(user_name_sender, password);
-//                        }
-//                    });
-//
-//            // MimeMessage object.
-//            MimeMessage message = new MimeMessage(session);
-//
-//            // Set From Field: adding senders email to from field.
-//            message.setFrom(new InternetAddress(user_name_sender));
-//
-//            // Set To Field: adding recipient's email to from field.
-//            message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
-//
-//            // Set Subject: subject of the email
-//            message.setSubject("Email Verification");
-//
-//            // set body of the email.
-//            message.setText("This is your verification link " + link);
-//
-//            // Send email.
-//            Transport.send(message);
-//            System.out.println("Mail successfully sent");
-//        } catch (Exception e) {
-//            System.out.println("Mail successfully sent " + e.getMessage());
-//        }
+        try {
+            // creating session object to get properties
+
+
+            Session session = Session.getDefaultInstance(props,
+                    new Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(user_name_sender, password);
+                        }
+                    });
+
+            // MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From Field: adding senders email to from field.
+            message.setFrom(new InternetAddress(user_name_sender));
+
+            // Set To Field: adding recipient's email to from field.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+
+            // Set Subject: subject of the email
+            message.setSubject("Email Verification");
+
+            // set body of the email.
+            message.setText("This is your verification link " + link);
+
+            // Send email.
+            Transport.send(message);
+            System.out.println("Mail successfully sent");
+        } catch (Exception e) {
+            System.out.println("Mail successfully sent " + e.getMessage());
+        }
     }
 
 }
